a8f97bc0b93337fa87638fdbfac01bfde456fd45
mixtaper <mixtaper@student.21-school.ru>
2026-09-16 16:39:22 +0300

    develop

---
+++ build/CMakeFiles/route_planner.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/route_planner.dir/cpp/src/main.cpp.o"
+  "CMakeFiles/route_planner.dir/cpp/src/main.cpp.o.d"
+  "route_planner"
+  "route_planner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/route_planner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
